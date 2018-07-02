19687e2ec8fc34aaf43ac9b15990f35afd6e5852
zxt <zxt@megvii.com>
2018-07-02 12:12:33 +0800

    Return more error info.

--- util.go
+++ util.go
@@ -18,10 +18,11 @@ func Shell(cmd string) (stdout string, stderr string, err error) {
 func Out(cmd string) (str string, err error) {
 	str, stderr, err := Shell(cmd)
 	if err != nil {
+		err = fmt.Errorf("%s\n%v", stderr, err)
 		return
 	}
 	if stderr != "" {
-		err = fmt.Errorf(stderr)
+		err = fmt.Errorf("%s\n", stderr)
 		return
 	}
 	return
