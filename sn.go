ef8c0442c7d66c7e2523b2a9e70ba3b0edaf8fe9
zxt <zxt@megvii.com>
2018-11-13 20:22:36 +0800

    Add new shell command.

--- sn.go
+++ sn.go
@@ -3,7 +3,7 @@ package shell
 import "strings"
 
 func Sn() (string, error) {
-	//dmidecode -s system-serial-number | grep -v \# | head -1 | sed s/[^a-zA-Z0-9]//g | tr a-z A-Z
+	//dmidecode -s system-serial-number | grep -v \# | head -1 | sed s/[^a-zA-Z0-9\.]//g | tr a-z A-Z
 	str, err := Out(`dmidecode -s system-serial-number | grep -v \# | head -1 | sed s/[^a-zA-Z0-9]//g`)
 	if err != nil {
 		return "", err
--- util.go
+++ util.go
@@ -6,6 +6,16 @@ import (
 	"os/exec"
 )
 
+func ShellContext(ctx, cmd string) (stdout string, stderr string, err error) {
+	c := exec.CommandContext(ctx, "bash", "-c", cmd)
+	var o, e bytes.Buffer
+	c.Stdout = &o
+	c.Stderr = &e
+	err = c.Run()
+	return o.String(), e.String(), err
+	return
+}
+
 func Shell(cmd string) (stdout string, stderr string, err error) {
 	c := exec.Command("bash", "-c", cmd)
 	var o, e bytes.Buffer
@@ -13,6 +23,19 @@ func Shell(cmd string) (stdout string, stderr string, err error) {
 	c.Stderr = &e
 	err = c.Run()
 	return o.String(), e.String(), err
+}
+
+func OutContext(ctx, cmd string) (str string, err error) {
+	str, stderr, err := ShellContext(cmd)
+	if err != nil {
+		err = fmt.Errorf("%s\n%s\n%v", str, stderr, err)
+		return
+	}
+	if stderr != "" {
+		err = fmt.Errorf("%s\n%s", str, stderr)
+		return
+	}
+	return
 }
 
 func Out(cmd string) (str string, err error) {
