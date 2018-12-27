b425196ac27f4959e9c66fc9cccb132657cd8bbc
zxt <zxt@megvii.com>
2018-12-27 21:10:15 +0800

    Fix kill child-process.

--- util.go
+++ util.go
@@ -1,20 +1,27 @@
 package shell
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"fmt"
 	"os/exec"
+	"syscall"
+	"time"
 )
 
 func ShellContext(ctx context.Context, cmd string) (stdout string, stderr string, err error) {
 	c := exec.CommandContext(ctx, "bash", "-c", cmd)
+	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	go func() {
+		<-ctx.Done()
+		syscall.Kill(-c.Process.Pid, syscall.SIGKILL)
+	}()
 	var o, e bytes.Buffer
 	c.Stdout = &o
 	c.Stderr = &e
 	err = c.Run()
 	return o.String(), e.String(), err
-	return
 }
 
 func Shell(cmd string) (stdout string, stderr string, err error) {
@@ -49,5 +56,69 @@ func Out(cmd string) (str string, err error) {
 		err = fmt.Errorf("%s\n%s", str, stderr)
 		return
 	}
+	return
+}
+
+////////////////////////////////////////////////////////////////
+func runWithTimeout(cmd string, num int) *exec.Cmd {
+	c := exec.Command("bash", "-c", cmd)
+	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	time.AfterFunc(time.Duration(num)*time.Millisecond, func() {
+		syscall.Kill(-c.Process.Pid, syscall.SIGKILL)
+	})
+	return c
+}
+
+func OutWithTimeout(cmd string, num int) (string, error) {
+	c := runWithTimeout(cmd, num)
+
+	out, err := c.Output()
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
+}
+func CombinedOutWithTimeout(cmd string, num int) (string, error) {
+	c := runWithTimeout(cmd, num)
+
+	out, err := c.CombinedOutput()
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), err
+}
+
+////////////////////////////////////////////////////////
+// ch,err:=shell.Run(ctx,cmd)
+//
+func Run(ctx context.Context, strcmd string) (ch chan string, err error) {
+
+	cmd := exec.Command("stdbuf", "-o0", "-e0", "bash", "-c", strcmd)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	go func() {
+		<-ctx.Done()
+		syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	}()
+
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+
+	ch = make(chan string)
+	go func() {
+		defer close(ch)
+		defer cmd.Wait()
+		scanner := bufio.NewScanner(out)
+		for scanner.Scan() {
+			ch <- scanner.Text()
+		}
+	}()
 	return
 }
--- util_test.go
+++ util_test.go
@@ -2,6 +2,7 @@ package shell
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -22,6 +23,17 @@ func TestOut(t *testing.T) {
 	assert := assert.New(t)
 
 	stdout, err := Out(`grep -io ABC <(echo -ne '123abc!@#\n$' | grep -P '\d+')`)
+
+	assert.Nil(err, "exit code")
+	assert.Equal("abc\n", stdout)
+}
+
+func TestOutWithTimeout(t *testing.T) {
+	assert := assert.New(t)
+
+	a := time.Now()
+	stdout, err := OutWithTimeout("sleep 3", 500)
+	t.Log(time.Now().Sub(a))
 
 	assert.Nil(err, "exit code")
 	assert.Equal("abc\n", stdout)
